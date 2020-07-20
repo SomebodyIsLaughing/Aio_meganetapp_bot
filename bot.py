a53ebd39ea557f1f47a2a3dbd71ad26125a70f6c
SomebodyIsLaughing <37039600+SomebodyIsLaughing@users.noreply.github.com>
2020-07-20 22:27:43 +0300

    main

--- bot.py
+++ bot.py
@@ -1,19 +1,27 @@
 from config import TOKEN
 from aiogram import Bot, Dispatcher, executor, types
 import keyboards as kb
-import hlp as hl
+import help as hp
+
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start'])
+# Клавиатура - mysupport
+@dp.message_handler(commands=['mysupport'])
 async def process_command_1(message: types.Message):
-    await message.reply("В чем вопрос?", reply_markup=kb.inline_kb1)
+    await message.reply("В чем вопрос? ", reply_markup=kb.inline_kb1)
 
+# клавиатура ONTAP
+@dp.message_handler(commands=['ontap_basic'])
+async def process_command_2(message: types.Message):
+    await message.reply("Лучшие категории, для лучших людей \U0001F60E", reply_markup=kb.ontap_kb1)
+
+# Помощь
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply(hl.help_message)
+    await message.reply(hp.help_message)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
--- keyboards.py
+++ keyboards.py
@@ -1,11 +1,21 @@
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
+import emoji
 
 # Кнопки поддержки по работе с mysupport
 inline_btn_1 = InlineKeyboardButton('Проверить гарантию', callback_data='button1', url='https://anton-kyrylenko.gitbook.io/tets/')
-inline_btn_2 = InlineKeyboardButton('Открыть кейс', callback_data='button2', url='https://app.gitbook.com/@anton-kyrylenko/s/tets/obrashenie-v-tekh.-podderzhku-netapp/~/settings/advanced')
-inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1,inline_btn_2)
+inline_btn_2 = InlineKeyboardButton('Открыть кейс', callback_data='button2', url='https://app.gitbook.com/@anton-kyrylenko/s/tets/obrashenie-v-tekh.-podderzhku-netapp')
+inline_btn_3 = InlineKeyboardButton('Рег. системы', callback_data='button3', url='')
+#inline_btn_4 = InlineKeyboardButton('Открыть кейс', callback_data='button2', url='')
+#inline_btn_5 = InlineKeyboardButton('Открыть кейс', callback_data='button2', url='')
+inline_kb1 = InlineKeyboardMarkup(row_width=2).add(inline_btn_1,inline_btn_2,inline_btn_3)
 
-# конпки поддержки по работе с Volume
+# конпки поддержки по работе с ontap_basic
+ontap_btn_1 = InlineKeyboardButton('Как бновить ПО', callback_data='button1', url='')
+ontap_btn_2 = InlineKeyboardButton('Обновить сертификат', callback_data='button2', url='')
+ontap_btn_3 = InlineKeyboardButton('Восстановить пароль', callback_data='button2', url='')
+#inline_btn_4 = InlineKeyboardButton('Открыть кейс', callback_data='button2', url='')
+#inline_btn_5 = InlineKeyboardButton('Открыть кейс', callback_data='button2', url='')
+ontap_kb1 = InlineKeyboardMarkup(row_width=2).add(ontap_btn_1,ontap_btn_2,ontap_btn_3)
 
