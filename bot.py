e243f1200cecda3f117d4bfb43a94a09a1a25120
SomebodyIsLaughing <37039600+SomebodyIsLaughing@users.noreply.github.com>
2020-07-23 12:34:22 +0300

    ы

--- bot.py
+++ bot.py
@@ -7,6 +7,10 @@ import help as hp
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+# Помощь
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply(hp.help_message)
 
 # Клавиатура - mysupport
 @dp.message_handler(commands=['mysupport'])
@@ -18,10 +22,26 @@ async def process_command_1(message: types.Message):
 async def process_command_2(message: types.Message):
     await message.reply("Лучшие категории, для лучших людей \U0001F60E", reply_markup=kb.ontap_kb1)
 
-# Помощь
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply(hp.help_message)
+# клавиатура ONTAP_special
+@dp.message_handler(commands=['ontap_special'])
+async def process_command_3(message: types.Message):
+    await message.reply("Лучшие категории, для лучших людей \U0001F60E", reply_markup=kb.spec_kb1)
+
+# клавиатура e-series
+@dp.message_handler(commands=['eseries'])
+async def process_command_4(message: types.Message):
+    await message.reply("Лучшие категории, для лучших людей \U0001F60E", reply_markup=kb.eseris_kb1)
+
+# клавиатура ONTAP
+@dp.message_handler(commands=['portfolio'])
+async def process_command_5(message: types.Message):
+    await message.reply("Лучшие категории, для лучших людей \U0001F60E", reply_markup=kb.port_kb1)
+
+# клавиатура ONTAP
+@dp.message_handler(commands=['design'])
+async def process_command_6(message: types.Message):
+    await message.reply("Лучшие категории, для лучших людей \U0001F60E", reply_markup=kb.design_kb1)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
