26436d1b36a33763da2894a742c0accbe87c4618
SomebodyIsLaughing <37039600+SomebodyIsLaughing@users.noreply.github.com>
2020-07-24 10:56:06 +0300

    ы

--- bot.py
+++ bot.py
@@ -15,32 +15,32 @@ async def process_help_command(message: types.Message):
 # Клавиатура - mysupport
 @dp.message_handler(commands=['mysupport'])
 async def process_command_1(message: types.Message):
-    await message.reply("В чем вопрос? ", reply_markup=kb.inline_kb1)
+    await message.reply("Все, что касатется операций с аккаунтом, обращений в тех. поддержку и полезных сервисов", reply_markup=kb.inline_kb1)
 
-# клавиатура ONTAP
+# клавиатура ONTAP_basic
 @dp.message_handler(commands=['ontap_basic'])
 async def process_command_2(message: types.Message):
-    await message.reply("Лучшие категории, для лучших людей \U0001F60E", reply_markup=kb.ontap_kb1)
+    await message.reply("Базовые операции ONTAP: Скачать прошивку, обновить прошивку, настроить Autosupport", reply_markup=kb.ontap_kb1)
 
 # клавиатура ONTAP_special
 @dp.message_handler(commands=['ontap_special'])
 async def process_command_3(message: types.Message):
-    await message.reply("Лучшие категории, для лучших людей \U0001F60E", reply_markup=kb.spec_kb1)
+    await message.reply("🚀 coming soon 🚀", reply_markup=kb.spec_kb1)
 
 # клавиатура e-series
 @dp.message_handler(commands=['eseries'])
 async def process_command_4(message: types.Message):
-    await message.reply("Лучшие категории, для лучших людей \U0001F60E", reply_markup=kb.eseris_kb1)
+    await message.reply("Все, что касатется настройки и администрирования ПО Santricity", reply_markup=kb.eseris_kb1)
 
-# клавиатура ONTAP
+# клавиатура portfolio
 @dp.message_handler(commands=['portfolio'])
 async def process_command_5(message: types.Message):
-    await message.reply("Лучшие категории, для лучших людей \U0001F60E", reply_markup=kb.port_kb1)
+    await message.reply("Какие продукты есть в портфолио NetApp, что они из себя представляют и их характеристики", reply_markup=kb.port_kb1)
 
-# клавиатура ONTAP
+# клавиатура design
 @dp.message_handler(commands=['design'])
 async def process_command_6(message: types.Message):
-    await message.reply("Лучшие категории, для лучших людей \U0001F60E", reply_markup=kb.design_kb1)
+    await message.reply("🚀 coming soon 🚀", reply_markup=kb.design_kb1)
 
 
 if __name__ == '__main__':
